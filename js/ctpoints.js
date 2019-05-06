eb97b69ed2bedb557e024e203107d7d375052697
shivamnaik10 <49772077+shivamnaik10@users.noreply.github.com>
2019-05-06 15:37:28 -0400

    Add files via upload

---
+++ js/ctpoints.js
@@ -0,0 +1,21 @@
+
+
+
+var myScroll;
+function loaded(){
+myScroll = new iScroll('scroll', {checkDOMChanges:true});
+}
+document.addEventListener('DOMContentLoaded', loaded, false)
+
+
+$(document).ready(function(){
+	init();
+});
+
+
+
+
+function init(){
+		
+}
+
